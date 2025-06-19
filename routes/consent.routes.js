d12bee620eca1c8c2d8efe014f2a98688d0fe0d3
CarCo53 <111662982+CarCo53@users.noreply.github.com>
2025-06-19 16:39:29 +0300

    Delete routes directory

--- routes/consent.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { listConsents } = require('../controllers/consent.controller');
-const { authenticateToken, authorizeRoles } = require('../middleware/auth');
-
-// Sadece admin/patron tüm rıza kayıtlarını görebilir
-router.get('/', authenticateToken, authorizeRoles("admin", "patron"), listConsents);
-
-module.exports = router;
--- routes/customer.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCustomer } = require('../controllers/customer.controller');
-const { authenticateToken } = require('../middleware/auth');
-
-router.post('/', authenticateToken, createCustomer);
-
-module.exports = router;
