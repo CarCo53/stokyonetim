012aa2131443afbce2a147face498cdffcbed763
CarCo53 <111662982+CarCo53@users.noreply.github.com>
2025-06-22 16:37:12 +0300

    Delete src directory

--- src/middlewares/rateLimit.js
+++
@@ -1,6 +0,0 @@
-const rateLimit = require('express-rate-limit');
-module.exports = rateLimit({
-    windowMs: 60 * 1000,
-    max: 100,
-    message: { message: 'Çok fazla istek, lütfen daha sonra tekrar deneyin.' }
-});
