6bdcc9e096eed2f81ab70bdba0490908cdde24c3
CarCo53 <111662982+CarCo53@users.noreply.github.com>
2025-07-03 17:59:32 +0300

    Delete src directory

--- src/config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'supersecretdevkey',
-    port: process.env.PORT || 3001,
-    dbFile: 'database.sqlite'
-};
