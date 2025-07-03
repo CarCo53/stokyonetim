8a1f9b7f0d12a10cbe2865e56b01509f54cdf6c9
CarCo53 <111662982+CarCo53@users.noreply.github.com>
2025-07-03 17:59:45 +0300

    Delete test-panel eskiden eski directory

--- test-panel eskiden eski/common.js
+++
@@ -1,5 +0,0 @@
-// Ortak fonksiyon: Header'a JWT token ekle
-function authHeaders() {
-  const token = localStorage.getItem('jwt_token');
-  return token ? { 'Authorization': 'Bearer ' + token } : {};
-}
