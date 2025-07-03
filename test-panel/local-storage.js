0ca8739039008485fd5ce9dc342667cc2bc68b27
CarCo53 <111662982+CarCo53@users.noreply.github.com>
2025-07-03 17:59:55 +0300

    Delete test-panel directory

--- test-panel/local-storage.js
+++
@@ -1,9 +0,0 @@
-function api() {
-  return localStorage.getItem('apiurl') || "http://localhost:3001";
-}
-function getHeaders() {
-  const h = { 'Content-Type': 'application/json' };
-  const token = localStorage.getItem('jwt_token');
-  if (token) h['Authorization'] = 'Bearer ' + token;
-  return h;
-}
